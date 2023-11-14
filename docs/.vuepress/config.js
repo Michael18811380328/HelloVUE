9398e0ead99ffd5b0295215ea688b39bee7eba7c
Michael An <2331806369@qq.com>
2023-11-14 16:46:32 +0800

    add vue press 01

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+// https://www.vuepress.cn/config/
+
+module.exports = {
+  title: 'Vue 笔记',
+  description: 'Just playing around',
+  // https://www.vuepress.cn/guide/markdown.html#%E8%A1%8C%E5%8F%B7
+  markdown: {
+    lineNumbers: true
+  },
+  // https://www.vuepress.cn/config/#%E6%B5%8F%E8%A7%88%E5%99%A8%E5%85%BC%E5%AE%B9%E6%80%A7
+  evergreen: true
+}
---
+++ vuepress/basic.md
@@ -0,0 +1,5 @@
+基本使用
+
+https://www.vuepress.cn/guide/directory-structure.html
+
+https://www.vuepress.cn/guide/basic-config.html
